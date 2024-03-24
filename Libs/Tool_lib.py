b3e203b6cf4673892006b5bf6ddc2bc36f9f8616
Hanfeng23333 <3065613494@qq.com>
2024-03-24 19:46:04 +0800

    update

--- Libs/Tool_lib.py
+++ Libs/Tool_lib.py
@@ -21,8 +21,22 @@ def establish_connection(start:tuple,end:tuple) -> list:
     middle_y = (start[1]+end[1])/2
     res = [[start,(start[0],middle_y)],[(end[0],middle_y),end]]
     if start[0] != end[0]:
-        res.append(sorted([(start[0],middle_y),(end[0],middle_y)],key=lambda x:x[0]))
+        res.append([(start[0],middle_y),(end[0],middle_y)])
     return res
+
+def image_selected(surface:pygame.Surface,color:tuple=(255,215,0),size=3):
+    width,height = surface.get_size()
+    res_image = surface.copy()
+    pygame.draw.line(res_image,color,(0,0),(width/4,0),size)
+    pygame.draw.line(res_image,color,(0,0),(0,height/4),size)
+    pygame.draw.line(res_image,color,(0,height-1),(0,height*3/4),size)
+    pygame.draw.line(res_image,color,(0,height-1),(width/4,height-1),size)
+    pygame.draw.line(res_image,color,(width-1,0),(width*3/4,0),size)
+    pygame.draw.line(res_image,color,(width-1,0),(width-1,height/4),size)
+    pygame.draw.line(res_image,color,(width-1,height-1),(width-1,height*3/4),size)
+    pygame.draw.line(res_image,color,(width-1,height-1),(width*3/4,height-1),size)
+    return res_image
+
 
 class Text_box:
     def __init__(self,font:pygame.font.Font,width:int=0,color:tuple=(255,255,255)):
@@ -30,7 +44,7 @@ class Text_box:
         self.font = font
         self.width = width
         self.color = color
-        self.image = None
+        self.image = pygame.Surface([width,0])
 
     def update(self):
         #create multiple lines of string
@@ -52,7 +66,6 @@ class Text_box:
         self.image = pygame.Surface([self.width if self.width else max_width,height_per_line*len(str_list)])
         for i,line in enumerate(str_list):
             self.image.blit(self.font.render(line,True,self.color),(0,height_per_line*i))
-        
     
     def set_text(self,text:str):
         self.text = text
@@ -104,5 +117,46 @@ class Scroll_box:
         self.image.blit(image_crop,(0,0))
 
     def set_image(self,image:pygame.Surface):
-        self.image = image
+        self.master_image = image.copy()
+        self.update()
+
+class Button_box:
+    def __init__(self,image:pygame.Surface,rect:pygame.Rect=None,keep_clicked:bool=False,highlight:bool=False):
+        self.master_image = image
+        self.rect = rect if rect else image.get_rect()
+        self.image = pygame.Surface(self.rect.size)
+        self.keep_clicked = keep_clicked
+        self.highlight = highlight
+        self.on_click = False
+
+        if not image.get_rect() in self.rect:
+            raise OverflowError("The rect offered is smaller than the rect of the image!")
+        
+    def update(self,mouse_pos:tuple=(-1,-1),mouse_click:tuple=(False,False)) -> bool:
+        clicked = False
+        self.image.fill((0,0,0))
+        mouse_rect = pygame.Rect(mouse_pos,(0,0))
+        if mouse_click[1]:
+            self.on_click = False
+        elif mouse_click[0]:
+            if mouse_rect in self.rect:
+                clicked = True
+                self.on_click = self.keep_clicked
+            else:
+                self.on_click = False
+        self.image.blit(self.master_image,((self.rect.width-self.master_image.get_width())/2,(self.rect.height-self.master_image.get_height())/2))
+        if (mouse_rect in self.rect and self.highlight) or self.on_click:
+            self.image = image_selected(self.image)
+        return clicked
+
+    def set_rect(self,rect:pygame.Rect):
+        self.rect = rect if rect else self.master_image.get_rect()
+        if not self.master_image.get_rect() in rect:
+            raise OverflowError("The rect offered is smaller than the rect of the image!")
+        self.update()
+        
+    def set_image(self,image:pygame.Surface):
+        self.master_image = image.copy()
+        if not image.get_rect() in self.rect:
+            self.rect = image.get_rect()
         self.update()
--- demo.py
+++ demo.py
@@ -11,40 +11,11 @@ screen = pygame.display.set_mode((1600,1200))
 pygame.display.set_caption("Demo")
 
 font = pygame.font.SysFont("simhei",20)
-"""
-test = [Skill_tree() for i in range(10)]
-for i in range(len(test)):
-    test[i].name = "%d"%i
-    image = pygame.Surface((50,50))
-    image.fill(tuple(map(lambda x:(x+1)*20,(i,i,i))))
-    text = font.render(test[i].name,True,(255,255,255))
-    image.blit(text,(10,10))
-    test[i].image = image
-
-test[0].set_next(test[1])
-test[0].set_next(test[2])
-test[0].set_next(test[4])
-test[1].set_next(test[3])
-test[2].set_next(test[3])
-test[4].set_next(test[5])
-test[6].set_last(test[3])
-test[6].set_last(test[5])
-
-screen.blit(test[0].make_tree(),(0,0))
-
-"""
-text = """喜欢test的原因可以有很多，足够写出长长的小作文来阐述自己的喜爱
-喜欢test的原因也可以只有一个，就是，我喜欢test
-每个人都有自己的喜欢，我选择的喜欢是test
-喜欢很简单，难的是坚持这份喜欢
-人的一生很长也很短，可以和test一起度过的时光也只是人生路上的一段
-但是在这一段时光里，我喜欢test，我会一直喜欢test"""
 
 test = Text_box(font,170)
-test.set_text(text)
-
-
 test_plus = Scroll_box(test.image,pygame.Rect(0,0,170,200))
+
+buttons = [Button_box(pygame.image.load("%d.png"%i),keep_clicked=True,highlight=True) for i in range(1,6)]
 
 while True:
     y_rel = 0
@@ -56,9 +27,20 @@ while True:
             y_rel = event.y
     
     mouse_x,mouse_y = pygame.mouse.get_pos()
-    test_plus.update((mouse_x-900,mouse_y-20),y_rel)
+    left_mouse,_,right_mouse = pygame.mouse.get_pressed()
+    target = -1
 
-    screen.blit(test.image,(20,20))
-    screen.blit(test_plus.image,(900,20))
+    for i in range(5):
+        buttons[i].update((mouse_x-1000,mouse_y-150*(i+1)),(left_mouse,right_mouse))
+        if buttons[i].on_click:
+            target = i
+    test.set_text(texts[target])
+    test_plus.set_image(test.image)
+    test_plus.update((mouse_x-20,mouse_y-20),y_rel)
+
+    screen.fill((0,0,0))
+    for i in range(5):
+        screen.blit(buttons[i].image,(1000,150*(i+1)))
+    screen.blit(test_plus.image,(100,100))
 
     pygame.display.update()
